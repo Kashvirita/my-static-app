96bef0edf060725611156c12eb777cc98e63ef86
Kashvirita <143584197+Kashvirita@users.noreply.github.com>
2024-06-17 16:05:25 +0000

    back to normal

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 function App() {
   const value = 'Kashvi,here';
-  return <div>Hello {value}</div>;
+  return <div>
+    <h1>Hello {value}</h1>
+    <p>This is my React static Web application</p>
+    <p>I study Btech in IT.</p>
+  </div>;
 }
 
 export default App;
